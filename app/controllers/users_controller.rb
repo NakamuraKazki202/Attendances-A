2ad9768f072acdef3daf632ad0f2edf756945fff
NakamuraKazki202 <fifth.class.points.uesugi@gmail.com>
2021-08-18 10:52:10 +0900

    Finish add-user-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,16 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_one_month, only: :show
 
   def index
     @users = User.paginate(page: params[:page])
   end
 
   def show
+    @worked_sum = @attendances.where.not(started_at: nil).count
   end
-
+  
   def new
     @user = User.new
   end
@@ -54,7 +56,7 @@ class UsersController < ApplicationController
       flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
     end
     redirect_to users_url
-  end  
+  end
 
   private
 
@@ -92,4 +94,3 @@ class UsersController < ApplicationController
       redirect_to root_url unless current_user.admin?
     end
 end
-
