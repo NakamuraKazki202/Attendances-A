9e55d0557407a36261ce38c2f7fd6512634f9875
NakamuraKazki202 <fifth.class.points.uesugi@gmail.com>
2021-08-27 13:20:55 +0900

    change adminheader

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   get '/signup', to: 'users#new'
-
+  get '/show' , to: 'users#show'
   
   # ログイン機能
   get    '/login', to: 'sessions#new'
